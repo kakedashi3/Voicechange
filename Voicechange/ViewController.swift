61f8f5f270a461b022b28b3c1c151e08f056d1fe
tatsuoka <69132449+Pabulow2060@users.noreply.github.com>
2021-09-06 06:23:37 +0900

    2021/09/06 morning

--- Voicechange/ViewController.swift
+++ Voicechange/ViewController.swift
@@ -13,18 +13,41 @@ final class ViewController: UIViewController {
     var audio = Audio()
     
     
-    @IBOutlet weak var speedSlider: UISlider!
+    @IBOutlet weak var speedSlider: UISlider! {
+        didSet {
+            speedSlider.minimumValue = 0.5
+            speedSlider.minimumValue = 1.5
+            speedSlider.setValue(1, animated: true)
+            speedSlider.tintColor = .blue
+        }
+    }
+    
+    @IBOutlet weak var pictchSlider: UISlider! {
+        didSet {
+            pictchSlider.minimumValue = -1000
+            pictchSlider.minimumValue = 1000
+            pictchSlider.setValue(0, animated: true)
+            pictchSlider.tintColor = .blue
+        }
+    }
     
     
+    @IBOutlet weak var echoSwitch: UISwitch! {
+        didSet {
+            echoSwitch.isOn = false
+            echoSwitch.onTintColor = .blue
+            echoSwitch.tintColor = .blue
+        }
+    }
     
-    @IBOutlet weak var pictchSlider: UISlider!
     
-    
-    
-    @IBOutlet weak var echoSwitch: UISwitch!
-    
-    
-    @IBOutlet weak var reverbSwitch: UISwitch!
+    @IBOutlet weak var reverbSwitch: UISwitch! {
+        didSet {
+            reverbSwitch.isOn = false
+            reverbSwitch.onTintColor = .blue
+            reverbSwitch.tintColor = .blue
+        }
+    }
     
     
     @IBOutlet weak var recordButton: UIButton!
@@ -43,8 +66,12 @@ final class ViewController: UIViewController {
 
     
     
+    @IBAction func record(_ sender: Any) {
+    }
     
     
+    @IBAction func play(_ sender: Any) {
+    }
     
 
 }
