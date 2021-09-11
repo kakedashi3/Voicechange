8d02852ccf4f11fd3a1b94cbb65ea1ef2b978cb8
tatsuoka <69132449+Pabulow2060@users.noreply.github.com>
2021-09-12 00:50:19 +0900

    2021/09/11

---
+++ Voicechange/ghostVoiceController.swift
@@ -0,0 +1,22 @@
+//
+//  ghostVoiceController.swift
+//  Voicechange
+//
+//  Created by 立岡力 on 2021/09/12.
+//
+
+import UIKit
+
+class ghostVoiceController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+    
+    
+
+}
